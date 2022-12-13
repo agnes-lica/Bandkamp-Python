85d8e34a119734c5f67aa47b219710a58b33aa52
Agnes-lica <agnes.lica@gmail.com>
2022-12-13 17:38:27 -0400

    feat: Making Tarefa 1

--- users/models.py
+++ users/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    username=models.CharField(max_length=128,unique=True)
+    email=models.EmailField(max_length=254,unique=True)
